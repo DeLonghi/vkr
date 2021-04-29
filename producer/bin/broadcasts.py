ca2288726daf07536f3bf0aa10eca97107aabbb6
Ilya <vinnikov_i@mail.ru>
2021-04-29 03:18:48 +0300

    Now producer read for .csv

--- producer/bin/broadcasts.py
+++ producer/bin/broadcasts.py
@@ -1,6 +1,6 @@
 # Standard library imports
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
+# from kafka import KafkaProducer
+# from kafka.errors import KafkaError
 
 from abc import abstractmethod
 from threading import Thread
@@ -17,10 +17,10 @@ class Broadcast(Thread):
     buffer: pd.DataFrame  # Буфер входных значений из csv
     json: dict  # json строка для отправки в топик
 
-    def __init__(self, id):
+    def __init__(self):
         Thread.__init__(self)
         self.json = {}
-        self.producer = KafkaProducer(bootstrap_servers=['localhost:29092'], api_version=(0, 10, 2), request_timeout_ms=10)
+        # self.producer = KafkaProducer(bootstrap_servers=['localhost:29092'], api_version=(0, 10, 2), request_timeout_ms=10)
 
 
     @abstractmethod
@@ -34,9 +34,9 @@ class Broadcast(Thread):
 
 class ModelBroadcast(Broadcast):
 
-    def __init__(self, id, delay, input):
-        self.buffer = pd.DataFrame(input)
-        Broadcast.__init__(self, id)
+    def __init__(self, delay, data_file):
+        self.buffer = pd.read_csv(data_file)
+        Broadcast.__init__(self)
         self.delay = delay
 
         # На первое время хардкодим и предполагаем что поток один. Также предполагаем что данне в csv для одного потока.
